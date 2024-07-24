3ea82fb1ebc7edd96b8d6087a35d73382253ca41
JiMo20 <Jmouhtasseb@gmail.com>
2024-07-24 10:47:09 +0400

    added sign in page

--- FoodApp.js
+++ FoodApp.js
@@ -6,19 +6,22 @@ let url = "https://food-delivery-backend-fcxs.onrender.com/api";
 
 let userSignUp = async (user) => {
   //Do fetch for user register url
+  //with the url specify the method name
+  //with the url specify the headers needed
+  //Finally send the body: which is the new user object
   let response = await fetch(`${url}/user/register`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(user),
   });
   //Convert the response to json
-
-  let Data = response.json();
+  let Data = await response.json();
+  if (Data.success) {
+    window.open("./SignIn.html");
+  } else {
+    alert(Data.message);
+  }
   console.log(Data, "Done!");
-
-  //with the url specify the method name
-  //with the url specify the headers needed
-  //Finally send the body: which is the new user object
 
   // let response = fetch(`${url}/user/register`,{
   //     method: "POST",
